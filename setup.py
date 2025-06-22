63d51ee077eca3577a270532fa68e77c7596b047
kashish shrivastav <en.shrivastavkashish@gmail.com>
2025-06-22 16:38:47 +0530

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="forestfire_sim",
+    version="0.1.0",
+    author="Your Name",
+    description="A library for simulating and predicting forest fire spread using AI/ML",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "rasterio",
+        "xarray",
+        "matplotlib",
+        "scikit-learn",
+        "pandas"
+    ],
+    python_requires=">=3.8",
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
